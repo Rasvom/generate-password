642ab43e5e97cac4b28540dee62432bb9bfb367c
Rasvom <rasvomb@gmail.com>
2023-05-19 14:29:47 +0300

    uniquePasswords

--- generate-password.js
+++ generate-password.js
@@ -6,57 +6,47 @@ function generatePassword(length, complexity, numberOfPasswords) {
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
   let passwords = [];
-
-  if (complexity === "easy") {
-    let pass = "";
-    for (let i = 0; i < numberOfPasswords; i++) {
-      for (let i = 0; i < length; i++) {
-        pass += alphabet[Math.floor(Math.random() * alphabet.length)];
-      }
-      if (passwords.includes(pass)) {
-        numberOfPasswords++;
+  for (let i = 0; [...new Set(passwords)].length < numberOfPasswords; i++) {
+    passwords = [];
+    if (complexity === "easy") {
+      let pass = "";
+      for (let i = 0; i < numberOfPasswords; i++) {
+        for (let i = 0; i < length; i++) {
+          pass += alphabet[Math.floor(Math.random() * alphabet.length)];
+        }
+        passwords.push(pass);
         pass = "";
-      } else {
+      }
+    } else if (complexity === "medium") {
+      let digitsAlphabet = digits + alphabet;
+      let pass = "";
+      for (let i = 0; i < numberOfPasswords; i++) {
+        for (let i = 0; i < length; i++) {
+          pass +=
+            digitsAlphabet[Math.floor(Math.random() * digitsAlphabet.length)];
+        }
+        passwords.push(pass);
+        pass = "";
+      }
+    } else if (complexity === "hard") {
+      let symbolsDigitsAlphabet = symbols + digits + alphabet;
+      let pass = "";
+      for (let i = 0; i < numberOfPasswords; i++) {
+        for (let i = 0; i < length; i++) {
+          pass +=
+            symbolsDigitsAlphabet[
+              Math.floor(Math.random() * symbolsDigitsAlphabet.length)
+            ];
+        }
         passwords.push(pass);
         pass = "";
       }
     }
-  } else if (complexity === "medium") {
-    let digitsAlphabet = digits + alphabet;
-    let pass = "";
-    for (let i = 0; i < numberOfPasswords; i++) {
-      for (let i = 0; i < length; i++) {
-        pass +=
-          digitsAlphabet[Math.floor(Math.random() * digitsAlphabet.length)];
-      }
-      if (passwords.includes(pass)) {
-        numberOfPasswords++;
-        pass = "";
-      } else {
-        passwords.push(pass);
-        pass = "";
-      }
-    }
-  } else if (complexity === "hard") {
-    let symbolsDigitsAlphabet = symbols + digits + alphabet;
-    let pass = "";
-    for (let i = 0; i < numberOfPasswords; i++) {
-      for (let i = 0; i < length; i++) {
-        pass +=
-          symbolsDigitsAlphabet[
-            Math.floor(Math.random() * symbolsDigitsAlphabet.length)
-          ];
-      }
-      if (passwords.includes(pass)) {
-        numberOfPasswords++;
-        pass = "";
-      } else {
-        passwords.push(pass);
-        pass = "";
-      }
+    if (i === 10) {
+      break;
     }
   }
   return [...new Set(passwords)];
 }
 
-console.log(generatePassword(10, "easy"));
+console.log(generatePassword(5, "easy", 5000));
